f0a802166fa0a0e87ddd793d00023b2c63c8009b
Muniskhon2025 <munisxonpersonal@gmail.com>
2025-02-20 20:38:57 +0500

    files added

---
+++ lesson-5/homework/task3.py
@@ -0,0 +1,12 @@
+def find_factors(n: int):
+    """Prints the factors of a given positive integer."""
+    for i in range(1, n + 1):
+        if n % i == 0:
+            print(f"{i} is a factor of {n}")
+
+num = int(input("Enter a positive integer: "))
+
+if num > 0:
+    find_factors(num)
+else:
+    print("Please enter a positive integer.")
